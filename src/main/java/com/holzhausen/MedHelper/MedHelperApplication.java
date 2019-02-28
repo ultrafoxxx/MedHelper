4477fc09f5eb86025a89e4b5134e592c4460895a
bigfoxxx <ph.cool97@gmail.com>
2019-02-28 18:47:54 +0100

    Szkielet projektu. Pierwszy commit

---
+++ src/main/java/com/holzhausen/MedHelper/MedHelperApplication.java
@@ -0,0 +1,13 @@
+package com.holzhausen.MedHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedHelperApplication.class, args);
+	}
+
+}

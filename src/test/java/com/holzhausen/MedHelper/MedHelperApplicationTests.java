da285de97028dfdee8fd2b2226c1a1cd8326c59e
bigfoxxx <ph.cool97@gmail.com>
2019-02-28 18:47:54 +0100

    Szkielet projektu. Pierwszy commit

---
+++ src/test/java/com/holzhausen/MedHelper/MedHelperApplicationTests.java
@@ -0,0 +1,16 @@
+package com.holzhausen.MedHelper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MedHelperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

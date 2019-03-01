c626b915962cf209063829838b69ad5b68a6aa58
bigfoxxx <ph.cool97@gmail.com>
2019-03-01 14:06:44 +0100

    Uzupełnienie klasy User.
    Klasa User została uzupełniona o ID, login i hasło użytkownika.
    Zostały także wygenerowane gettery i settery.

--- src/main/java/com/holzhausen/MedHelper/model/User.java
+++ src/main/java/com/holzhausen/MedHelper/model/User.java
@@ -1,4 +1,39 @@
 package com.holzhausen.MedHelper.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
 public class User {
+
+    private int id;
+
+    private String login;
+
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }

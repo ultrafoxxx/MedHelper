3a2ad3024f332c286a678c277a1a156b7c2f4b01
bigfoxxx <ph.cool97@gmail.com>
2019-03-08 18:06:01 +0100

    Dodano klasę Placowka.
    Dodano także asocjację do klasy placowka do klasy GabinetLekarski.

--- src/main/java/com/holzhausen/MedHelper/model/entities/GabinetLekarski.java
+++ src/main/java/com/holzhausen/MedHelper/model/entities/GabinetLekarski.java
@@ -1,6 +1,7 @@
 package com.holzhausen.MedHelper.model.entities;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.*;
+import com.holzhausen.MedHelper.util.EntityResolver;
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.LazyCollection;
 import org.hibernate.annotations.LazyCollectionOption;
@@ -11,7 +12,7 @@ import java.util.List;
 
 @Entity
 @Proxy(lazy = false)
-@Table(name = "Wizyta")
+@Table(name = "GabinetLekarski")
 public class GabinetLekarski {
 
     @Id
@@ -27,6 +28,16 @@ public class GabinetLekarski {
     @Cascade({org.hibernate.annotations.CascadeType.SAVE_UPDATE, org.hibernate.annotations.CascadeType.LOCK})
     @LazyCollection(LazyCollectionOption.TRUE)
     private List<Wizyta> wizyty;
+
+    @JsonBackReference
+    @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id",
+            resolver = EntityResolver.class, scope = Placowka.class)
+    @JsonIdentityReference(alwaysAsId = true)
+    @ManyToOne(targetEntity = Placowka.class, fetch = FetchType.LAZY)
+    @Cascade({org.hibernate.annotations.CascadeType.LOCK})
+    @JoinColumns(value = {@JoinColumn(name = "placowkaId", referencedColumnName = "id", nullable = false)},
+            foreignKey = @ForeignKey(name = "gabinet_placowkaFK"))
+    private Placowka placowka;
 
     public int getId() {
         return id;
@@ -50,5 +61,13 @@ public class GabinetLekarski {
 
     public void setWizyty(List<Wizyta> wizyty) {
         this.wizyty = wizyty;
+    }
+
+    public Placowka getPlacowka() {
+        return placowka;
+    }
+
+    public void setPlacowka(Placowka placowka) {
+        this.placowka = placowka;
     }
 }
---
+++ src/main/java/com/holzhausen/MedHelper/model/entities/Placowka.java
@@ -0,0 +1,87 @@
+package com.holzhausen.MedHelper.model.entities;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+import org.hibernate.annotations.Proxy;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Proxy(lazy = false)
+@Table(name = "Placowka")
+public class Placowka {
+
+    @Id
+    @GeneratedValue(generator = "PlacowkaIdGenerator")
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @Column(name = "miasto", nullable = false)
+    private String miasto;
+
+    @Column(name = "adres", nullable = false)
+    private String adres;
+
+    @Column(name = "DlugoscGeo", nullable = false)
+    private double dlugoscGeo;
+
+    @Column(name = "SzerGeo", nullable = false)
+    private double szerGeo;
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "placowka", targetEntity = GabinetLekarski.class)
+    @Cascade({org.hibernate.annotations.CascadeType.SAVE_UPDATE, org.hibernate.annotations.CascadeType.LOCK})
+    @LazyCollection(LazyCollectionOption.TRUE)
+    private List<GabinetLekarski> gabinetyLekarskie;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMiasto() {
+        return miasto;
+    }
+
+    public void setMiasto(String miasto) {
+        this.miasto = miasto;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
+    public double getDlugoscGeo() {
+        return dlugoscGeo;
+    }
+
+    public void setDlugoscGeo(double dlugoscGeo) {
+        this.dlugoscGeo = dlugoscGeo;
+    }
+
+    public double getSzerGeo() {
+        return szerGeo;
+    }
+
+    public void setSzerGeo(double szerGeo) {
+        this.szerGeo = szerGeo;
+    }
+
+    public List<GabinetLekarski> getGabinetyLekarskie() {
+        return gabinetyLekarskie;
+    }
+
+    public void setGabinetyLekarskie(List<GabinetLekarski> gabinetyLekarskie) {
+        this.gabinetyLekarskie = gabinetyLekarskie;
+    }
+}

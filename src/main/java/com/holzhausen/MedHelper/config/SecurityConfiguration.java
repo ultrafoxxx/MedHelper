0a3674e79121890411f2db5aba4ebe71e17f6742
bigfoxxx <ph.cool97@gmail.com>
2019-03-15 16:44:58 +0100

    Dodano i zmodyfikowano piki związane z autoryzacją

--- src/main/java/com/holzhausen/MedHelper/config/SecurityConfiguration.java
+++ src/main/java/com/holzhausen/MedHelper/config/SecurityConfiguration.java
@@ -1,20 +1,68 @@
 package com.holzhausen.MedHelper.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private BCryptPasswordEncoder passwordEncoder;
+
+    private DataSource dataSource;
+
+    @Value("$(spring.queries.users-query)")
+    private String userQuery;
+
+    @Value("$(spring.queries.role-query)")
+    private String roleQuery;
+
+    @Autowired
+    public SecurityConfiguration(BCryptPasswordEncoder passwordEncoder, DataSource dataSource) {
+        this.passwordEncoder = passwordEncoder;
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth){
+
+        try{
+            auth.jdbcAuthentication()
+                    .usersByUsernameQuery(userQuery)
+                    .authoritiesByUsernameQuery(roleQuery)
+                    .dataSource(dataSource)
+                    .passwordEncoder(passwordEncoder);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
 
     @Override
     protected void configure(HttpSecurity http){
 
         try {
             http.authorizeRequests()
-                    .antMatchers("/").permitAll();
+                    .antMatchers("/").permitAll()
+                    .antMatchers("/login").permitAll()
+                    .anyRequest().authenticated().and().csrf()
+                    .disable().formLogin()
+                    .loginPage("/login").failureUrl("/login?error=true")
+                    .defaultSuccessUrl("/")
+                    .usernameParameter("email")
+                    .passwordParameter("password")
+                    .and().logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .logoutSuccessUrl("/");
         } catch (Exception e){
             e.printStackTrace();
         }

a9ae86a33d09412af956d06dee81b6975de88124
bigfoxxx <ph.cool97@gmail.com>
2019-03-15 19:07:42 +0100

    Dodanie autentykacji użytkownika.

--- src/main/java/com/holzhausen/MedHelper/controller/LogowanieController.java
+++ src/main/java/com/holzhausen/MedHelper/controller/LogowanieController.java
@@ -1,8 +1,12 @@
 package com.holzhausen.MedHelper.controller;
 
+import com.holzhausen.MedHelper.model.entities.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Controller
 public class LogowanieController {

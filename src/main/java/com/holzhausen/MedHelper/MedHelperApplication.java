edea8ef33d84500da8d675c56f171c1a2fce968f
bigfoxxx <ph.cool97@gmail.com>
2019-03-18 18:53:21 +0100

    Mail

--- src/main/java/com/holzhausen/MedHelper/MedHelperApplication.java
+++ src/main/java/com/holzhausen/MedHelper/MedHelperApplication.java
@@ -1,5 +1,6 @@
 package com.holzhausen.MedHelper;
 
+import com.holzhausen.MedHelper.util.EmailServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
--- src/main/java/com/holzhausen/MedHelper/config/WebMvcConfig.java
+++ src/main/java/com/holzhausen/MedHelper/config/WebMvcConfig.java
@@ -2,8 +2,12 @@ package com.holzhausen.MedHelper.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Properties;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -12,5 +16,23 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+//    @Bean
+//    public JavaMailSender getJavaMailSender() {
+//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+//        mailSender.setHost("localhost");
+//        mailSender.setPort(8005);
+//
+//        mailSender.setUsername("medhelper@medhelper.com");
+//        mailSender.setPassword("CoolHolz8425");
+//
+//        Properties props = mailSender.getJavaMailProperties();
+//        props.put("mail.transport.protocol", "smtp");
+//        props.put("mail.smtp.auth", "true");
+//        props.put("mail.smtp.starttls.enable", "true");
+//        props.put("mail.debug", "true");
+//
+//        return mailSender;
+//    }
 
 }
